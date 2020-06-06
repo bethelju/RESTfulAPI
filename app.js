147c160db137507f754eb32ae09313c3c321639b
justib41 <46506710+bethelju@users.noreply.github.com>
2020-06-05 20:04:47 -0700

    Added handlebars, nodemon, and login page

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const hbs = require('express-handlebars');
+
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars')
 
 app.use('/', require('./routes/index'));
+
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,4 @@ const router = module.exports = require('express').Router();
 
 router.use('/lodgings', require('./lodgingRoutes'));
 router.use('/guests', require('./guestsRoutes'));
+router.use('/login', require('./loginRoutes'))
